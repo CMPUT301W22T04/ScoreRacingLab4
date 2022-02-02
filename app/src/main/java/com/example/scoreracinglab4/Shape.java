40890418fee26283f63dc6153347e44049808a5e
Kin Jiengphhattanakit <kinkin159@hotmail.com>
2022-02-01 19:49:15 -0700

    Create Project

---
+++ app/src/main/java/com/example/scoreracinglab4/Shape.java
@@ -0,0 +1,11 @@
+package com.example.scoreracinglab4;
+
+public class Shape {
+    private int x;
+    private int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
