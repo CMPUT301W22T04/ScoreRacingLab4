aab606f910735359afae179d8c88e02d56c195d4
Allen Obiacoro <nobiacor@ualberta.ca>
2022-02-03 01:54:33 -0700

    Adding Color String Field to Shape Class

--- app/src/main/java/com/example/scoreracinglab4/Shape.java
+++ app/src/main/java/com/example/scoreracinglab4/Shape.java
@@ -3,9 +3,11 @@ package com.example.scoreracinglab4;
 public class Shape {
     private int x;
     private int y;
+    String color = "Maroon";
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+
     }
 }
