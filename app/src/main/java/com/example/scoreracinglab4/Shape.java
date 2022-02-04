158dabc65c32efc018647a7eafacb78953909c83
Allen Obiacoro <nobiacor@ualberta.ca>
2022-02-04 03:59:40 -0700

    Resolve Conflict

--- app/src/main/java/com/example/scoreracinglab4/Shape.java
+++ app/src/main/java/com/example/scoreracinglab4/Shape.java
@@ -3,6 +3,7 @@ package com.example.scoreracinglab4;
 public class Shape {
     private int x;
     private int y;
+    private String color = "Green";
 
     public Shape(int x, int y) {
         this.x = x;
