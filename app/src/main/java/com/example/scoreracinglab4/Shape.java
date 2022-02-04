60e12acd68b8d00beaf06f6c86849f072fb67adf
Kieran <na>
2022-02-03 18:03:04 -0700

    Removed extra lines in conflict resolution

--- app/src/main/java/com/example/scoreracinglab4/Shape.java
+++ app/src/main/java/com/example/scoreracinglab4/Shape.java
@@ -3,11 +3,8 @@ package com.example.scoreracinglab4;
 public class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
     private String mycolor = "Obsidian";
-=======
     private String color = "Green";
->>>>>>> a13f97af4ee47ae52b9f425075e1b0a31d73ea95
 
     public Shape(int x, int y) {
         this.x = x;
